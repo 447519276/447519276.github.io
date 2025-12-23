7b9d7346d84ee635a74d9d413340e4a7a2a7b07d
qinhao <447519276@qq.com>
2025-12-23 10:58:33 +0800

    feat:增加样式文件

--- index.tsx
+++ index.tsx
@@ -1,16 +1,23 @@
+/*
+ * @Author: QinHao
+ * @Date: 2025-12-19 17:46:59
+ * @LastEditors: qinhao
+ * @LastEditTime: 2025-12-23 10:58:14
+ * @FilePath: \religious-imsd:\work\447519276.github.io\index.tsx
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+    throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
